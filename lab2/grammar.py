22f0722e4086ca7c76f767ef11ab8ccfe50dfe87
fairay <48217178+fairay@users.noreply.github.com>
2023-03-26 17:10:42 +0300

    added Chomsky Normal Form

--- lab2/grammar.py
+++ lab2/grammar.py
@@ -34,11 +34,6 @@ class NonTermSymbol(Symbol):
 
 
 @dataclass
-class TerminalSymbol(Symbol):
-    pass
-
-
-@dataclass
 class Production:
     lhs: Symbol | str
     rhs: list[Symbol] | list[str]
@@ -61,7 +56,12 @@ class Grammar:
         self.productions = [p for p in self.productions if p.lhs != s]
 
     def add_production(self, lhs: Symbol, rhs: list[Symbol]):
-        self.productions.append(Production(lhs, rhs))
+        other_rhs = [p.rhs for p in self.symbol_production(lhs)]
+        if rhs not in other_rhs:
+            self.productions.append(Production(lhs, rhs))
+
+    def remove_production(self, p: Production):
+        self.productions.remove(p)
 
     def symbol_names_dist(self):
         symbols = {}
@@ -86,13 +86,16 @@ class Grammar:
                 prod.rhs = [symbols[s.name] for s in prod.rhs]
 
     def print(self):
+        def rhs_str(rhs: list[Symbol]):
+            return "".join(rs.spell for rs in rhs) if rhs else "ε"
+
         for s in self.non_term_symbols:
             productions = self.symbol_production(s)
             if not productions:
                 continue
 
             print(s, "→", end=" ")
-            str_prods = ["".join(rs.spell for rs in p.rhs) for p in productions]
+            str_prods = [rhs_str(p.rhs) for p in productions]
             print("|".join(str_prods))
 
     def replace_previous_with_rhs(self, s: NonTermSymbol):
@@ -111,6 +114,22 @@ class Grammar:
 
         self.remove_symbol_productions(s)
         self.productions.extend(new_prods)
+
+    def _get_nt_by_name(self, name: str, spell: str):
+        for s in self.non_term_symbols:
+            if s.name == name:
+                return s
+        new_symbol = NonTermSymbol(name, spell)
+        self.non_term_symbols.append(new_symbol)
+        return new_symbol
+
+    def get_nonterm_analog(self, t_s: TermSymbol):
+        return self._get_nt_by_name(t_s.name + "_ALT", t_s.spell + "'")
+
+    def get_compressed_nt(self, symbols: list[Symbol]):
+        name = ''.join(s.name for s in symbols)
+        spell = ''.join(s.spell for s in symbols)
+        return self._get_nt_by_name(f'<{name}>', f'<{spell}>')
 
 
 def reduce_left_recursion(old_g: Grammar) -> Grammar:
@@ -133,7 +152,55 @@ def reduce_left_recursion(old_g: Grammar) -> Grammar:
         alt_s = NonTermSymbol(s.name + "'")
         g.non_term_symbols.append(alt_s)
         for rhs in beta_rhs:
+            g.add_production(s, rhs)
             g.add_production(s, rhs + [alt_s])
         for rhs in alpha_rhs:
+            g.add_production(alt_s, rhs)
             g.add_production(alt_s, rhs + [alt_s])
+    return g
+
+
+def find_eps_nonterms(g: Grammar) -> set[NonTermSymbol]:
+    terms: set[NonTermSymbol] = set()
+    while True:
+        new_terms: set[NonTermSymbol] = terms.copy()
+        for p in g.productions:
+            if set(p.rhs).issubset(terms):
+                new_terms.add(p.lhs)
+        if new_terms == terms:
+            break
+        else:
+            terms = new_terms
+    return terms
+
+def chomsky_normal_form(old_g: Grammar) -> Grammar:
+    g = deepcopy(old_g)
+
+    index = 0
+    while index < len(g.productions):
+        p = g.productions[index]
+        # S -> e
+        if p.lhs == g.start_symbol and len(p.rhs) == 0:
+            pass
+        # A -> a
+        elif len(p.rhs) == 1 and p.rhs[0].type == 'term':
+            pass
+        # A -> ??
+        elif len(p.rhs) == 2:
+            for i, s in enumerate(p.rhs):
+                if s.type == 'term':
+                    nt_s = g.get_nonterm_analog(s)
+                    g.add_production(nt_s, [s])
+                    p.rhs[i] = nt_s
+        # A -> ?...?
+        else:
+            lhs = p.lhs
+            for i in range(len(p.rhs)-2):
+                new_nt = g.get_compressed_nt(p.rhs[i+1:])
+                g.add_production(lhs, [p.rhs[i], new_nt])
+                lhs = new_nt
+            g.add_production(lhs, p.rhs[-2:])
+            g.remove_production(p)
+            index -= 1
+        index += 1
     return g
--- lab2/main.py
+++ lab2/main.py
@@ -1,10 +1,10 @@
-from grammar import Grammar, reduce_left_recursion
+from grammar import Grammar, reduce_left_recursion, chomsky_normal_form
 from dacite import from_dict
 import json
 
 
 def main():
-    with open("data3.json", "r") as f:
+    with open("data4.json", "r") as f:
         data = json.load(f)
     result = from_dict(data_class=Grammar, data=data["grammar"])
 
@@ -15,6 +15,9 @@ def main():
     g = reduce_left_recursion(result)
     g.print()
 
+    print("--------- Chomsky Normal Form -----------")
+    g = chomsky_normal_form(g)
+    g.print()
 
 if __name__ == "__main__":
     main()
